6d2181139eab214018087ba6946f9ba5c0a595f6
ZAHID GUL <98535146+zahidgul107@users.noreply.github.com>
2025-06-05 23:48:55 +0530

    added question view activity

--- Quiz App/app/src/main/java/com/example/quizapp/ui/QuestionsActivity.kt
+++ Quiz App/app/src/main/java/com/example/quizapp/ui/QuestionsActivity.kt
@@ -1,15 +1,40 @@
 package com.example.quizapp.ui
 
+import android.graphics.Color
+import android.graphics.Typeface
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.quizapp.R
+import com.example.quizapp.model.Question
 import com.example.quizapp.utils.Constants
 
-class QuestionsActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var progressBar : ProgressBar
+    private lateinit var textViewProgress : TextView
+    private lateinit var textViewQuestion : TextView
+    private lateinit var flagImage: ImageView
+    private lateinit var checkButton : Button
+
+    private lateinit var textViewOptionOne : TextView
+    private lateinit var textViewOptionTwo : TextView
+    private lateinit var textViewOptionThree : TextView
+    private lateinit var textViewOptionFour : TextView
+
+    private val currentPosition = 1
+    private lateinit var questionsList : MutableList<Question>
+    private var selectedOptionPosition = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,7 +45,94 @@ class QuestionsActivity : AppCompatActivity() {
             insets
         }
 
-        val questionsList = Constants.getQuestions()
+        progressBar = findViewById<ProgressBar>(R.id.progressBar)
+        textViewProgress = findViewById<TextView>(R.id.text_view_progress)
+        textViewQuestion = findViewById<TextView>(R.id.question_text_view)
+        flagImage = findViewById<ImageView>(R.id.image_flag)
+        checkButton = findViewById<Button>(R.id.button_check)
+
+        textViewOptionOne = findViewById<TextView>(R.id.text_view_option_one)
+        textViewOptionTwo = findViewById<TextView>(R.id.text_view_option_two)
+        textViewOptionThree = findViewById<TextView>(R.id.text_view_option_three)
+        textViewOptionFour = findViewById<TextView>(R.id.text_view_option_four)
+
+        textViewOptionOne.setOnClickListener(this)
+        textViewOptionTwo.setOnClickListener(this)
+        textViewOptionThree.setOnClickListener(this)
+        textViewOptionFour.setOnClickListener(this)
+        checkButton.setOnClickListener(this)
+
+        questionsList = Constants.getQuestions()
         Log.d("QuestionSize", "${questionsList.size}")
+
+        setQuestion()
+    }
+
+    private fun setQuestion() {
+        val question = questionsList[currentPosition - 1]
+        flagImage.setImageResource(question.image)
+        progressBar.progress = currentPosition
+        textViewProgress.text = "$currentPosition/${progressBar.max}"
+        textViewQuestion.text = question.question
+        textViewOptionOne.text = question.optionOne
+        textViewOptionTwo.text = question.optionTwo
+        textViewOptionThree.text = question.optionThree
+        textViewOptionFour.text = question.optionFour
+
+        if (currentPosition == questionsList.size) {
+             checkButton.text = "FINISH"
+        } else {
+            checkButton.text = "CHECK"
+        }
+    }
+
+    private fun resetOptions() {
+        val options = mutableListOf<TextView>()
+        options.add(textViewOptionOne)
+        options.add(textViewOptionTwo)
+        options.add(textViewOptionThree)
+        options.add(textViewOptionFour)
+
+        for (option in options) {
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_border_option_bg
+            )
+        }
+    }
+
+    private fun selectedOption(textView: TextView, selectOptionNumber: Int) {
+        resetOptions()
+
+        selectedOptionPosition = selectOptionNumber
+
+        textView.setTextColor(Color.parseColor("#363A43"))
+        textView.setTypeface(textView.typeface, Typeface.BOLD)
+        textView.background = ContextCompat.getDrawable(
+            this,
+            R.drawable.selected_option_border_option_bg
+        )
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.text_view_option_one -> {
+                selectedOption(textViewOptionOne, 1)
+            }
+            R.id.text_view_option_two -> {
+                selectedOption(textViewOptionTwo, 2)
+            }
+            R.id.text_view_option_three -> {
+                selectedOption(textViewOptionThree, 3)
+            }
+            R.id.text_view_option_four -> {
+                selectedOption(textViewOptionFour, 4)
+            }
+            R.id.button_check -> {
+                
+            }
+        }
     }
 }
